dae8d2bd666bdf3cfb1d5357bf1bcf329e1baddb
Laksh Kudnani <kundnanl@sheridancollege.ca>
2025-04-02 23:37:22 -0400

    build tweaks

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -2,13 +2,13 @@ import { appRouter } from "@/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { createContext } from "@/server/context";
 
-const handler = (req: Request) => {
-    return fetchRequestHandler({
-        endpoint: "/api/trpc",
-        router: appRouter,
-        req,
-        createContext,
-    });
+const handler = async (req: Request) => {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    router: appRouter,
+    req,
+    createContext,
+  });
 };
 
 export { handler as GET, handler as POST };
